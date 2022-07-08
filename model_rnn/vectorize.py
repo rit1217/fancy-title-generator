445c213ebbc339bfd05ea707c3c2b32002bc5159
Phurit Warapattanapong <57401346+rit1217@users.noreply.github.com>
2022-07-08 14:22:39 +0700

    Update model_rnn/vectorize.py
    Co-authored-by: Kirill Orlov <24492681+IDDT@users.noreply.github.com>

--- model_rnn/vectorize.py
+++ model_rnn/vectorize.py
@@ -4,7 +4,7 @@ from .preprocessor import CHAR_TO_IX
 
 
 def make_input_vect(title:str) -> torch.tensor:
-    char_ix_arr = []
-    for char in title:
-        char_ix_arr.append(CHAR_TO_IX[char])
-    return F.one_hot(torch.tensor(char_ix_arr),num_classes=len(CHAR_TO_IX)).type(torch.float32)
+    arr = torch.tensor([CHAR_TO_IX[c] for x in title],
+        dtype=torch.int64, device=torch.device('cpu'), requires_grad=False)
+    return F.one_hot(arr, num_classes=len(CHAR_TO_IX))\
+        .type(torch.float32)
