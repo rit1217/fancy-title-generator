a367b86fc4fdfdd97eb5d11e7d9c056995bdf6a7
Phurit Warapattanapong <phuritwarapat@gmail.com>
2022-07-22 14:02:58 +0700

    Remove redundant prefix traversal

--- model_rnn/adapter.py
+++ model_rnn/adapter.py
@@ -19,35 +19,35 @@ class ModelAdapter:
     def predict(self, prefix:str='', top_n:int=20, max_length:int=100) -> list:
         title = preprocess_text(prefix)[:-1]
         X = make_input_vect(title)
-        results = []
-        for title_tensor, score in self._predict_helper(X, top_n, max_length):
-            results.append({'title': devectorize(title_tensor), 'score': score})
-        results.sort(key=lambda item: item['score'], reverse=True)
-
-        return results[:top_n]
-
-    def _predict_helper(self, X:torch.tensor, n:int=20, max_length:int=100, prefix_score:float=0) -> Generator[Tuple, None, None]:        
-        score = prefix_score
-        title_len = X.size()[0]
         with torch.no_grad():
             hidden = None
-            for i in range(title_len - 1):
+            for i in range(len(title) - 1):
                 output, hidden = self.rnn.predict(X[-1].reshape(1, 1, -1), hidden)
-            for i in range(max_length - title_len):
-                output, hidden = self.rnn.predict(X[-1].reshape(1, 1, -1), hidden)
-                topv, topi = output.reshape(-1).topk(2)
-                top_char_ix = topi[0].item()
-                top_2_char_ix = topi[1].item()
+            results = []
+            for title_tensor, score in self._predict_helper(X, hidden, top_n, max_length):
+                results.append({'title': devectorize(title_tensor), 'score': score})
+            results.sort(key=lambda item: item['score'], reverse=True)
+        return results[:top_n]
 
-                if n > 0 and top_2_char_ix != CHAR_TO_IX[EOS]:
-                    n = n // 2
-                    new_X = concat_one_hot(X, top_2_char_ix)
-                    for result in self._predict_helper(new_X, n, max_length, score + topv[1].item()):
-                        yield result
+    def _predict_helper(self, X:torch.tensor, hidden:torch.tensor=None, n:int=20, max_length:int=100, prefix_score:float=0) -> Generator[Tuple, None, None]:        
+        score = prefix_score
+        title_len = X.size()[0]
+        
+        for i in range(max_length - title_len):
+            output, hidden = self.rnn.predict(X[-1].reshape(1, 1, -1), hidden)
+            topv, topi = output.reshape(-1).topk(2)
+            top_char_ix = topi[0].item()
+            top_2_char_ix = topi[1].item()
 
-                score += topv[0].item()
-                if top_char_ix == CHAR_TO_IX[EOS]:
-                    break
-                X = concat_one_hot(X, top_char_ix)
+            if n > 0 and top_2_char_ix != CHAR_TO_IX[EOS]:
+                n = n // 2
+                new_X = concat_one_hot(X, top_2_char_ix)
+                for result in self._predict_helper(new_X, hidden, n, max_length, score + topv[1].item()):
+                    yield result
 
-            yield X[1:], score
+            score += topv[0].item()
+            if top_char_ix == CHAR_TO_IX[EOS]:
+                break
+            X = concat_one_hot(X, top_char_ix)
+
+        yield X[1:], score
